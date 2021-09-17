a745be8dd08cfa5d82c48209c2cc3f88caebb549
Hideki Yoshikura <evenintherain@gmail.com>
2021-09-18 00:11:22 +0900

    add

---
+++ src/MultiYoutubeViewer/MultiYoutubeViewer/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace MultiYoutubeViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1日目",
+            "2日目"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "トラック0",
+            "トラック1",
+            "トラック2",
+            "トラック3",
+            "トラック4",
+            "トラック5",
+            "トラック6",
+            "グラレコ1",
+            "グラレコ2",
+            ""});
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(75, 124);
+            this.listBox1.TabIndex = 7;
+            // 
+            // panel10
+            // 
+            this.panel10.Location = new System.Drawing.Point(93, 12);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(1070, 622);
+            this.panel10.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "切り替え";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1175, 646);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ src/MultiYoutubeViewer/MultiYoutubeViewer/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MultiYoutubeViewer
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, Dictionary<int, string>> _dic = new Dictionary<int, Dictionary<int, string>>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _dic.Clear();
+            _dic = new Dictionary<int, Dictionary<int, string>>
+            {
+                {
+                    0,
+                    new Dictionary<int, string>
+                    {
+                        { 0, "https://youtu.be/URtVSE1lnVk" },
+                        { 1, "https://youtu.be/bAEZtVOgFog" },
+                        { 2, "https://youtu.be/ACykeL3jR-c" },
+                        { 3, "https://youtu.be/QxEFHG1y8KU" },
+                        { 4, "https://youtu.be/oc1mTmRM7Ig" },
+                        { 5, "https://youtu.be/GLt0v8lm-_0" },
+                        { 6, "https://youtu.be/A5u9UhScgrU" },
+                        { 7, "https://youtu.be/RE3i7XytM2Q" },
+                        { 8, "https://youtu.be/MKRVK9ZAcZs" }
+                    }
+                },
+
+                {
+                    1,
+                    new Dictionary<int, string>
+                    {
+                        { 0, "https://youtu.be/jC6hlbEt2wI" },
+                        { 1, "https://youtu.be/E1VU8TlcSJg" },
+                        { 2, "https://youtu.be/gSUVNRStxLc" },
+                        { 3, "https://youtu.be/0j4pKEUz_gw" },
+                        { 4, "https://youtu.be/BQ0ZgSQREio" },
+                        { 5, "https://youtu.be/iEskt_o2jPw" },
+                        { 6, "https://youtu.be/vNH1cND8tSg" },
+                        { 7, "https://youtu.be/P3BnCDkGwe4" },
+                        { 8, "https://youtu.be/fEJ3LZK28D8" }
+                    }
+                }
+            };
+
+            comboBox1.SelectedIndex = 0;
+            listBox1.SelectedIndex = 0;
+
+        }
+
+        private void buttonEnd_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Environment.Exit(0);
+        }
+
+        private void Display(int day, IEnumerable<int> selected)
+        {
+
+            var totalWidth = panel10.Width;
+            var totalHeight = panel10.Height;
+
+            var count = selected.Count();
+            if (count == 0) { return; }
+
+            panel10.Controls.Clear();
+
+            if (count == 1)
+            {
+                var browser = new WebBrowser
+                {
+                    Width = totalWidth,
+                    Height = totalHeight,
+                    Url = new Uri(_dic[day][selected.Single()])
+                };
+                panel10.Controls.Add(browser);
+            }
+            else if (count <= 4)
+            {
+                var width = (totalWidth - 10) / 2;
+                var height = (totalHeight - 10) / 2;
+
+                for (int i = 0; i < count; i++)
+                {
+                    var browser = new WebBrowser
+                    {
+                        Width = width,
+                        Height = height,
+                        Url = new Uri(_dic[day][selected.ElementAt(i)])
+                    };
+                    panel10.Controls.Add(browser);
+                    browser.Top = i < 2 ? 0 : height + 5;
+                    browser.Left = i % 2 == 0 ? 0 : width + 5;
+                }
+            }
+            else
+            {
+                var width = (totalWidth - 15) / 3;
+                var height = (totalHeight - 15) / 3;
+
+                for (int i = 0; i < count; i++)
+                {
+                    var browser = new WebBrowser
+                    {
+                        Width = width,
+                        Height = height,
+                        Url = new Uri(_dic[day][selected.ElementAt(i)])
+                    };
+                    panel10.Controls.Add(browser);
+                    browser.Top = i < 3 ? 0 : (i < 6 ? height + 5 : (height + 5) * 2);
+                    browser.Left = i % 3 == 0 ? 0 : (i % 3 == 1 ? width + 5 : (width + 5) * 2);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var idx = comboBox1.SelectedIndex;
+
+            var selected = new List<int>();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (listBox1.GetSelected(i))
+                {
+                    selected.Add(i);
+                }
+            }
+
+            Display(idx, selected);
+
+        }
+    }
+}
